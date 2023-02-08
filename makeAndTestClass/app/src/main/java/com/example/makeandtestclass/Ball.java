70ff278ef1c9b17adc76c99d493203bc13ff28b1
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-02-07 18:52:32 -0700

    Add data to Ball class and run tests

--- makeAndTestClass/app/src/main/java/com/example/makeandtestclass/Ball.java
+++ makeAndTestClass/app/src/main/java/com/example/makeandtestclass/Ball.java
@@ -1,4 +1,72 @@
 package com.example.makeandtestclass;
 
-public class Ball {
+public class Ball implements AutoCloseable {
+
+    private boolean popped;
+    private String color;
+    private float bounceHeightInFeet;
+    private double weightInGrams;
+
+    Ball(String color, double weightInGrams, float bounceHeightInFeet, boolean popped) {
+        this.color = color;
+        this.weightInGrams = weightInGrams;
+        this.bounceHeightInFeet = bounceHeightInFeet;
+        this.popped = popped;
+    }
+
+    Ball(double weightInGrams) {
+        this("black", weightInGrams, 2.5f, false);
+    }
+
+    Ball(float bounceHeightInFeet) {
+        this("black", 150.00d, bounceHeightInFeet, false);
+    }
+
+    Ball(String color) {
+        this(color, 150.00d, 2.5f, false);
+    }
+
+    Ball() {
+        this("black", 150.0d, 2.5f, false);
+    }
+
+    public void close() {
+        System.out.println("closing ball " + this + " color " + color);
+    }
+
+    public boolean isPopped() {
+        return popped;
+    }
+
+    public void setPopped(boolean popped) {
+        this.popped = popped;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public float getBounceHeightInFeet() {
+        return bounceHeightInFeet;
+    }
+
+    public void setBounceHeightInFeet(float bounceHeightInFeet) {
+        this.bounceHeightInFeet = bounceHeightInFeet;
+    }
+
+    public double getWeightInGrams() {
+        return weightInGrams;
+    }
+
+    public void setWeightInGrams(double weightInGrams) {
+        if (weightInGrams > 0) {
+            this.weightInGrams = weightInGrams;
+        } else {
+            throw new IllegalArgumentException("weight must be positive");
+        }
+    }
 }
--- makeAndTestClass/app/src/test/java/com/example/makeandtestclass/BallTest.java
+++ makeAndTestClass/app/src/test/java/com/example/makeandtestclass/BallTest.java
@@ -1,4 +1,64 @@
+package com.example.makeandtestclass;
+
 import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class BallTest {
-  
+
+    @Test
+    public void close() {
+    }
+
+    @Test
+    public void isPoppedDefault() {
+        Ball ball = new Ball();
+        assertFalse(ball.isPopped());
+    }
+
+    public void isPoppedGetSet() {
+        Ball ball = new Ball();
+        ball.setPopped(true);
+        assertFalse(ball.isPopped());
+    }
+
+    @Test
+    public void setPopped() {
+    }
+
+    @Test
+    public void getColor() {
+    }
+
+    @Test
+    public void setColor() {
+    }
+
+    @Test
+    public void getBounceHeightInFeet() {
+    }
+
+    @Test
+    public void setBounceHeightInFeet() {
+    }
+
+    @Test
+    public void getWeightInGrams() {
+        Ball ball = new Ball();
+        assertEquals(150.0, ball.getWeightInGrams(), 0.001);
+    }
+
+    @Test
+    public void setWeightInGramsOk() {
+        Ball ball = new Ball();
+        ball.setWeightInGrams(150.0);
+        assertEquals(150.0, ball.getWeightInGrams(), 0.001);
+    }
+
+    @Test (expected = IllegalArgumentException.class)
+    public void setWeightInGramsFail() {
+        Ball ball = new Ball();
+        ball.setWeightInGrams(-33);
+        assertEquals(-33, ball.getWeightInGrams(), 0.001);
+    }
 }

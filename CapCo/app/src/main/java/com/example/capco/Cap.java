a21cd405b8f116da78168df40a7a7e0828abbb18
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-02-10 13:07:24 -0700

    JSON example

---
+++ CapCo/app/src/main/java/com/example/capco/Cap.java
@@ -0,0 +1,69 @@
+package com.example.capco;
+
+import android.content.ClipData;
+
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+
+import java.io.IOException;
+
+@JsonDeserialize(using = Cap.Deserializer.class)
+public class Cap {
+    public static class Deserializer extends JsonDeserializer<Cap> {
+    //{"size":"MEDIUM","label":"Ok"}
+        @Override
+        public Cap deserialize(JsonParser jp, DeserializationContext context) throws IOException {
+
+            JsonNode node = jp.getCodec().readTree(jp);
+            return new Cap(node);
+        }
+    }
+
+    // Size Cap.size = Size.MEDIUM;
+    public static final Size DEFAULT_SIZE = Size.MEDIUM;
+    private static Size size = DEFAULT_SIZE;
+    private String label = DEFAULT_LABEL;
+    public static final String DEFAULT_LABEL = "";
+
+    public Size getSize() {
+        return size;
+    }
+
+    public void setSize(Size size) {
+        if (size == null) {
+            throw new IllegalArgumentException("size cannot be null");
+        }
+        this.size = size;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        if (label == null) {
+            throw new IllegalArgumentException("label cannot be null");
+        }
+        if (Utils.codepoints(label) > 20) {
+            throw new IllegalArgumentException("label is too long");
+        }
+        this.label = label;
+    }
+
+    public Cap(JsonNode node) {
+        this.size =  Size.valueOf(node.get("size").asText());
+        this.label = node.get("label").asText();
+    }
+
+    public Cap(String label, Size size) {
+        this.label = label;
+        this.size = size;
+    }
+
+    public Cap() {
+        this(DEFAULT_LABEL, DEFAULT_SIZE);
+    }
+}
---
+++ CapCo/app/src/test/java/com/example/capco/CapTest.java
@@ -0,0 +1,67 @@
+package com.example.capco;
+
+import static org.junit.Assert.*;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CapTest {
+
+    Cap cap;
+    @Before
+    public void setUp() throws Exception {
+        cap = new Cap();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getSize() {
+        assertEquals(Cap.DEFAULT_SIZE, cap.getSize());
+    }
+
+    @Test
+    public void setSize() {
+        cap.setSize(Size.LARGE);
+        assertEquals(Size.LARGE, cap.getSize());
+    }
+
+    @Test
+    public void getLabel() {
+        assertEquals("", cap.getLabel());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setLabelNULL() {
+        cap.setLabel(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setLabelLong() {
+        cap.setLabel("123456789012345678901234567890");
+    }
+
+    @Test
+    public void setLabelOk() {
+        cap.setLabel("Ok");
+        assertEquals("Ok", cap.getLabel());
+    }
+
+    @Test
+    public void fromJSON() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\n" +
+                "    \"size\": \"LARGE\",\n" +
+                "    \"label\": \"My Label\"\n" +
+                "}";
+        cap = mapper.readValue(json, Cap.class);
+
+        assertEquals(Size.LARGE, cap.getSize());
+        assertEquals("My Label", cap.getLabel());
+    }
+}

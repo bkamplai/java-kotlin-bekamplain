e4090a4d603411d833e546f918bfba1e2a9f5bef
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-03-01 10:01:55 -0700

    strings

--- JavaBasics/app/src/main/java/com/example/javabasics/Cap.java
+++ JavaBasics/app/src/main/java/com/example/javabasics/Cap.java
@@ -1,6 +1,35 @@
 package com.example.javabasics;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Cap {
+
+    private int threadCount;
+
+    void doUselessThingsWithThreadCount() {
+        threadCount++;
+        //Integer x = new Integer(3);
+        //++x; // x = new Integer(x.intValue() + 1);
+
+        // C++: std::list < int > ints;
+        List< Integer > ints = new LinkedList< Integer >();
+
+        // C++: for (std::list<int>::iterator i > ints.begin(): i!=ints.end(); ++i) {cout << *i;)
+        // i = begin() -1;
+        Iterator<Integer> i = ints.iterator();
+        while (i.hasNext()) { // (i+1) != end())
+            // C++: ++i; x = *i;
+            Integer x = i.next();
+        }
+
+        // C++: for (auto &x : ints) {...}
+        for (Integer x : ints) {
+
+        }
+
+    }
 
     // Size Cap.size = Size.MEDIUM;
     public static final Size DEFAULT_SIZE = Size.MEDIUM;
@@ -20,6 +49,18 @@ public class Cap {
 
     public String getLabel() {
         return label;
+    }
+
+    public void shout(int loudness) {
+        //StringBuffer sb = new StringBuffer(label); //thread safe / slower version
+        StringBuilder sb = new StringBuilder(label.length() + loudness); // not thread safe / faster
+        sb.append(label);
+        for (int i =0; i < loudness; ++i) {
+            //label += "!";
+            //label = label + "!";
+            sb.append("!");
+        }
+        label = sb.toString();
     }
 
     public void setLabel(String label) {

c836cad66a1f11889529a33fe41466157e3083bd
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-03-16 23:24:15 -0600

    Converted Ball object to JSON

--- makeAndTestClass/app/src/main/java/com/example/makeandtestclass/Ball.java
+++ makeAndTestClass/app/src/main/java/com/example/makeandtestclass/Ball.java
@@ -1,11 +1,75 @@
 package com.example.makeandtestclass;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+
+import java.io.IOException;
+
+@JsonDeserialize(using = Ball.Deserializer.class)
+@JsonSerialize(using = Ball.Serializer.class)
 public class Ball implements AutoCloseable {
+    public static class Deserializer extends JsonDeserializer<Ball> {
+        @Override
+        public Ball deserialize(JsonParser jp, DeserializationContext context) throws IOException {
+            JsonNode node = jp.getCodec().readTree(jp);
+            return new Ball(node);
+        }
+    }
+
+    // https://www.baeldung.com/jackson-object-mapper-tutorial
+    public static class Serializer extends StdSerializer<Ball> {
+        public Serializer() {
+            this(null);
+        }
+
+        public Serializer(Class<Ball> t) {
+            super(t);
+        }
+        @Override
+        public void serialize(Ball ball, JsonGenerator jsonGenerator, SerializerProvider serializer) throws IOException {
+            ball.toJson(jsonGenerator);
+        }
+    }
 
     private boolean popped;
     private String color;
     private float bounceHeightInFeet;
     private double weightInGrams;
+
+    public Ball(JsonNode node) {
+        this.color = node.get("color").asText();
+        this.weightInGrams = node.get("weight").asDouble();
+        this.bounceHeightInFeet = node.get("height").asInt();
+        this.popped = node.get("popped").asBoolean();
+    }
+
+    public void toJson(JsonGenerator out) throws IOException {
+        out.writeStartObject();
+        out.writeStringField("color", getColor());
+        out.writeNumberField("weight", getWeightInGrams());
+        out.writeNumberField("height", getBounceHeightInFeet());
+        out.writeBooleanField("popped", isPopped());
+        out.writeEndObject();
+    }
+
+    public String toJson() {
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            return mapper.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     Ball(String color, double weightInGrams, float bounceHeightInFeet, boolean popped) {
         this.color = color;
--- makeAndTestClass/app/src/test/java/com/example/makeandtestclass/BallTest.java
+++ makeAndTestClass/app/src/test/java/com/example/makeandtestclass/BallTest.java
@@ -2,9 +2,25 @@ package com.example.makeandtestclass;
 
 import static org.junit.Assert.*;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class BallTest {
+
+    Ball ball;
+
+    @Before
+    public void setUp() throws Exception {
+        ball = new Ball();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
 
     @Test
     public void close() {
@@ -60,5 +76,36 @@ public class BallTest {
         Ball ball = new Ball();
         ball.setWeightInGrams(-33);
         assertEquals(-33, ball.getWeightInGrams(), 0.001);
+    }
+
+    @Test
+    public void fromJSON() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        String json = "{\n" +
+                "    \"weight\": 150.0,\n" +
+                "    \"color\": \"black\",\n" +
+                "    \"height\": 2.5,\n" +
+                "    \"popped\": false\n" +
+                "}";
+        ball = mapper.readValue(json, Ball.class);
+
+        assertEquals(150.0d, ball.getWeightInGrams(), 0.001);
+        assertEquals("black", ball.getColor());
+        assertEquals(2.5f, ball.getBounceHeightInFeet(), 0.5);
+        assertFalse(ball.isPopped());
+    }
+
+    // Ball() {
+    //        this("black", 150.0d, 2.5f, false);
+    //    }
+    @Test
+    public void toJSON() throws Exception {
+        ball = new Ball("black", 150.0d, 2.5f, false);
+
+        ObjectMapper mapper = new ObjectMapper();
+        String json  = mapper.writeValueAsString(ball);
+        String expect = "{\"color\":\"black\",\"weight\":150.0,\"height\":2.5,\"popped\":false}";
+
+        assertEquals(expect,json);
     }
 }

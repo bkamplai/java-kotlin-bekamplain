175103e4e837f811d57b94d61e7cb1f570da0617
Brandon Kamplain <103218065+bkamplai@users.noreply.github.com>
2023-03-28 19:19:05 -0600

    Threads/Cap Factory

--- Threads/app/src/main/java/commons/Cap.java
+++ Threads/app/src/main/java/commons/Cap.java
@@ -1,45 +1,9 @@
-package com.capco.commons;
-
-import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationContext;
-import com.fasterxml.jackson.databind.JsonDeserializer;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+package commons;
 
 import java.io.IOException;
 
 
-@JsonDeserialize(using = Cap.Deserializer.class)
-@JsonSerialize(using = Cap.Serializer.class)
 public class Cap {
-    public static class Deserializer extends JsonDeserializer<Cap> {
-        @Override
-        public Cap deserialize(JsonParser jp, DeserializationContext context) throws IOException {
-            JsonNode node = jp.getCodec().readTree(jp);
-            return new Cap(node);
-        }
-    }
-
-    // https://www.baeldung.com/jackson-object-mapper-tutorial
-    public static class Serializer extends StdSerializer<Cap> {
-        public Serializer() {
-            this(null);
-        }
-
-        public Serializer(Class<Cap> t) {
-            super(t);
-        }
-        @Override
-        public void serialize(Cap cap, JsonGenerator jsonGenerator, SerializerProvider serializer) throws IOException {
-            cap.toJson(jsonGenerator);
-        }
-    }
     public static final Size DEFAULT_SIZE = Size.MEDIUM;
 
     private static Size size = DEFAULT_SIZE;
@@ -70,18 +34,6 @@ public class Cap {
             throw new IllegalArgumentException("label is too long");
         }
         this.label = label;
-    }
-
-    public Cap(JsonNode node) {
-        this.size = Size.valueOf(node.get("size").asText());
-        this.label = node.get("label").asText();
-    }
-
-    public void toJson(JsonGenerator out) throws IOException {
-        out.writeStartObject();
-        out.writeStringField("size",getSize().toString());
-        out.writeStringField("label",getLabel());
-        out.writeEndObject();
     }
 
     public Cap(String label, Size size) {
--- Threads/app/src/main/java/commons/Size.java
+++ Threads/app/src/main/java/commons/Size.java
@@ -1,4 +1,4 @@
-package com.capco.commons;
+package commons;
 
 public enum Size {
     SMALL,
